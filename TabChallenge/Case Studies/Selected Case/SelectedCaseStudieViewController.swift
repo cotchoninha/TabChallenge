8927a94dbde8de678b06795eb4d7bd2bcf839a74
Marcela <cotchoninha@yahoo.com.br>
2020-06-06 16:27:43 +0100

    added xib for selected case study

---
+++ TabChallenge/Case Studies/Selected Case/SelectedCaseStudieViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SelectedCaseStudieViewController.swift
+//  TabChallenge
+//
+//  Created by Spare on 06/06/2020.
+//  Copyright © 2020 Spare. All rights reserved.
+//
+
+import UIKit
+
+class SelectedCaseStudieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
